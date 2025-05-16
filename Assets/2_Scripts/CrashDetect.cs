cdcc51cf306b98fc8d280e7fe60d47479a7ea29b
박주현 <vahyeon2501@sonline20.sen.go.kr>
2025-05-16 15:07:23 +0900

    Restart Level

---
+++ Assets/2_Scripts/CrashDetect.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CrashDetect : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            Debug.Log("오 내 머리야");
+        }
+    }
+}
---
+++ Assets/2_Scripts/FinishLIne.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class FinishLIne : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("완주했습니다!");
+        }
+    }
+}
