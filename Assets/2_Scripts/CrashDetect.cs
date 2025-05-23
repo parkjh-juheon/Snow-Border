0b5445284a684982c9384882282e5f69d1ac0811
박주현 <vahyeon2501@sonline20.sen.go.kr>
2025-05-23 15:21:00 +0900

    Invoke

--- Assets/2_Scripts/CrashDetect.cs
+++ Assets/2_Scripts/CrashDetect.cs
@@ -3,12 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetect : MonoBehaviour
 {
+    [SerializeField] private float reloadDelay = 2f;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Ground"))
         {
             Debug.Log("오 내 머리야");
-            SceneManager.LoadScene(0);
+            Invoke(nameof(ReloadScene), reloadDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/2_Scripts/FinishLIne.cs
+++ Assets/2_Scripts/FinishLIne.cs
@@ -3,12 +3,24 @@ using UnityEngine.SceneManagement;
 
 public class FinishLIne : MonoBehaviour
 {
+    [SerializeField] private float reloadDelay = 2f;
+    [SerializeField] private ParticleSystem finishEffect; // Finish Effect 파티클 시스템 참조
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             Debug.Log("완주했습니다!");
-            SceneManager.LoadScene(0);
+            if (finishEffect != null)
+            {
+                finishEffect.Play(); // 파티클 재생
+            }
+            Invoke(nameof(ReloadScene), reloadDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
